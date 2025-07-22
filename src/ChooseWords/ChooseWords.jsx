c3b9acc40c5733c6d623f52eb47f46bf1fa212b4
Gabriel Mazuru <gabrielmazuru6@gmail.com>
2025-07-22 15:35:36 +0300

    Css for the word page done

--- src/ChooseWords/ChooseWords.jsx
+++ src/ChooseWords/ChooseWords.jsx
@@ -2,14 +2,14 @@ import styles from "./ChooseWords.module.css";
 
 function ChooseWords() {
   return (
-    <div>
-      <h1>Type Test</h1>
-      <p>Choose the length of the test</p>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Type Test</h1>
+      <p className={styles.desc}>Choose the length of the test</p>
       <div>
         <label>Words : </label>
         <input type="number" max={100} min={10} />
       </div>
-      <button>Start Test</button>
+      <button className={styles.startButton}>Start Test</button>
     </div>
   );
 }
